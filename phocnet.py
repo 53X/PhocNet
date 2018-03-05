03a0c654f73b2adc783afd65d41a011843c4a8c2
Dropout1997 <mirage_1997@mail.ru>
2018-03-06 03:33:26 +0530

    few updates

--- phocnet.py
+++ phocnet.py
@@ -1,16 +1,18 @@
 # library dependencies
 
-from keras.layers import Input, Conv2D , MaxPool2D , Dense 
+from keras.layers import Input, Conv2D , MaxPool2D , Dense , Dropout ,Lambda
+from spp import Spatial_Pyramidal_Pooling
+from keras.models import Model 
 
 # the architecture
 
 input_layer=Input(shape=(None,None,3),name='Input_Image')
 conv_1=Conv2D(filters=64,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_1',activation='relu')(input_layer)
 conv_2=Conv2D(filters=64,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_2',activation='relu')(conv_1)
-pool_layer_1=MaxPool2D(kernel_size=(2,2),strides=(2,2),name='pooling_1')(conv_2)
-conv_3=Conv2D(filters=128,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_3',activation='relu')(pool_layer)
+pool_layer_1=MaxPool2D(pool_size=(2,2),strides=(2,2),name='pooling_1',data_format='channels_last')(conv_2)
+conv_3=Conv2D(filters=128,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_3',activation='relu')(pool_layer_1)
 conv_4=Conv2D(filters=128,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_4',activation='relu')(conv_3)
-pool_layer_2=MaxPool2D(kernel_size=(2,2),strides=(2,2),name='pooling_2')(conv_4)
+pool_layer_2=MaxPool2D(pool_size=(2,2),strides=(2,2),name='pooling_2',data_format='channels_last')(conv_4)
 conv_5=Conv2D(filters=256,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_5',activation='relu')(pool_layer_2)
 conv_6=Conv2D(filters=256,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_6',activation='relu')(conv_5)
 conv_7=Conv2D(filters=256,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_7',activation='relu')(conv_6)
@@ -20,5 +22,14 @@ conv_10=Conv2D(filters=256,kernel_size=(3,3),padding='same',strides=(1,1),data_f
 conv_11=Conv2D(filters=512,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_11',activation='relu')(conv_10)
 conv_12=Conv2D(filters=512,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_12',activation='relu')(conv_11)
 conv_13=Conv2D(filters=512,kernel_size=(3,3),padding='same',strides=(1,1),data_format='channels_last',name='convolution_layer_13',activation='relu')(conv_12)
+spatial_pool=Lambda(Spatial_Pyramidal_Pooling.Pooling2D,arguments={'pool_levels':[3,2,1]})(conv_13)
+fully_connected_1=Dense(4096,activation='relu')(spatial_pool)
+Dropout_layer_1=Dropout(0.5,name='Dropout_Layer_1')(fully_connected_1)
+fully_connected_2=Dense(4096,activation='relu')(conv_13)
+Dropout_layer_2=Dropout(0.5,name='Dropout_layer_2')(fully_connected_2)
+output_layer=Dense(604,activation='sigmoid')(Dropout_layer_2)
+
+model=Model(inputs=input_layer,outputs=conv_13)
+model.summary()
 
 
--- spp.py
+++ spp.py
@@ -1,30 +1,23 @@
 # library dependencies
 
 from keras.layers import MaxPool2D 
-from keras.layers import  Flatten
-from keras.layers import concatenate
+import keras.backend as K
 import numpy as np 
-from numpy import ceil
-from numpy import floor
 
 
 class Spatial_Pyramidal_Pooling:
 
 	def Pooling2D(input_tensor, pool_levels):
+		pool_list=[]
 		shape=input_tensor.shape
-		individual_pool_tensors=[]
-		for n in pool_levels:
-			window_height=ceil(int(shape[1])/n)
-			window_width=ceil(int(shape[2])/n)
-			stride_height=floor(int(shape[1])/n)
-			stride_width=floor(int(shape[2])/n)
-			level_pool=MaxPool2D(kernel_size=(window_height,window_width),strides=(stride_height,stride_width),name='pool_level_'+str(n))(input_tensor)
-			flattened_level_pool=Flatten(name='flattening_level_'+str(n))(level_pool)
-			individual_pool_tensors.append(flattened_level_pool)
-		output_tensor=concatenate(individual_pool_tensors,axis=-1)
+		for level in pool_levels:
+			window_height=np.ceil(int(shape[1])/level)
+			window_width=np.ceil(int(shape[2])/level)
+			stride_height=np.floor(int(shape[1])/level)
+			stride_width=np.floor(int(shape[2])/level)
+			pooling=MaxPool2D(pool_size=(window_height,window_width),strides=(stride_height,stride_width))(input_tensor)
+			flattened_tensor=K.flatten(pooling)
+			pool_list.append(flattened_tensor)
+		output_tensor=K.concatenate(pool_list,axis=-1)
 		return(output_tensor)
-
-
-
-
 
