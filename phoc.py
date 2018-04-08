699f5af7251af418ca9cd7101e2cca20391f66c0
53X <mirage_1997@mail.ru>
2018-04-09 00:57:25 +0530

    Bigram Features added --- 604

--- phoc.py
+++ phoc.py
@@ -1,68 +1,63 @@
 import numpy as np
+import more_itertools as mit 
+import re
 
 alphabet=[chr(i) for i in range(97,123)]
 digits=[chr(i) for i in range(48,58)]
-bigrams=[ 'th' , 'he'  ,'in'  ,'er' , 'an'  ,'re'  ,'on'  ,'at'  ,'en'  ,'nd'  ,
-          'ti' ,'es'  ,'or'  ,'te'  ,'of'  ,'ed'  ,'is'  ,'it'  ,'al'   ,'ar'   ,
-          'st'   ,'to' ,'nt'   ,'ng'   ,'se'   ,'ha'   ,'as'   ,'ou'   ,'io'   ,
-          'le'   ,'ve'   ,'co'   ,'me'   ,'de'   ,'hi'   ,'ri'   ,'ro'   ,'ic'   ,
-          'ne'   ,'ea'   ,'ra'   ,'ce'   ,'li'   ,'ch'   ,'ll'   ,'be'   ,'ma'   ,'si'   ,'om'   ,'ur' ]
+bigrams=[ 'th' , 'he'  ,'in'  ,'er' , 'an'  ,'re'  ,'on'  ,'at'  ,'en'  ,'nd'  , 'ti' ,'es'  ,'or'  ,'te'  ,'of'  ,'ed'  ,'is'  ,'it'  ,'al'   ,'ar'   , 'st'   ,'to' ,'nt'   ,'ng'   ,'se'   ,'ha'   ,'as'   ,'ou'   ,'io'   ,'le'   ,'ve'   ,'co'   ,'me'   ,'de'   ,'hi'   ,'ri'   ,'ro'   ,'ic'   , 'ne'   ,'ea'   ,'ra'   ,'ce'   ,'li'   ,'ch'   ,'ll'   ,'be'   ,'ma'   ,'si'   ,'om'   ,'ur' ]
 
 
 
 
-	class develop_phoc():
+class develop_phoc():
 
-		#This function splits the string into many substrings as per the required level of split
+    #This function splits the string into many substrings as per the required level of split
 
-		
-		def split_string(string,level):
+        
+    def split_string(string,level):
 
-			beg,split_counter,jump,error=0,1,len(string)//split,len(string)%split
-			offset=jump if (jump>error) else error
-			end=offset
-			splits=[]
-			
-			while(split_counter<level):
-				substring=string[beg:end]
-				beg=end
-				end=end+offset
-				split_counter+=1
-				splits.sppend(substring)
-			
-			splits.append(string[beg:])
-			return(splits)	
+            iterable=list(string)
+            splits=[''.join(list(c)) for c in mit.divide(level,iterable)]
+
+            return(splits)  
 
 
-		#This function extacts the features from each substring :  36 dimensional  
+    #This function extacts the features from each substring :  36 dimensional / 86 dimensional(for level 2)
 
-		def extract_feature(sub_string,level):
+    def extract_feature(sub_string,level):
 
-			substring_feature=[]
-			alphabet_feature=[1 if element in sub_string else 0 for element in alphabet]
-			digits_feature=[1 if element in sub_string else 0 for element in digits]
-			substring_feature=final_feature+alphabet_feature+digits_feature+bigram_feature
-		
-			#BIGRAM FEATURE YET TO BE ADDED FOR LEVEL 2
+            substring_feature=[]
+            alphabet_feature=[1 if element in sub_string else 0 for element in alphabet]
+            digits_feature=[1 if element in sub_string else 0 for element in digits]
+            substring_feature=alphabet_feature+digits_feature
+        
+            #BIGRAM FEATURE ADDED FOR LEVEL 2
 
-			return(substring_feature)
-			
+            if(level==2):
+                bigram_feature=[1 if len(re.findall(b,sub_string))!=0 else 0 for b in bigrams]
+                substring_feature=substring_feature+bigram_feature
 
-		#This function combines the above two functions and returns the final PHOC representation
+            return(substring_feature)
+            
 
-		def phoc_representation(string):
+    #This function combines the above two functions and returns the final PHOC representation
 
-			split_levels=[2,3,4,5]
-			feature_vector=[]
-		
-			for split in split_levels:
-				substrings=develop_phoc.split_string(string,split)
-				for sub in sub_strings:
-					level_feature=develop_phoc.extract_feature(sub,split)
-				    feature_vector=feature_vector+level_feature
+    def phoc_representation(string):
 
-			return(np.array(feature_vector))	
+            split_levels=[2,3,4,5]
+            feature_vector=[]
+        
+            for split in split_levels:
+                substrings=develop_phoc.split_string(string,split)
+                for sub in substrings:
+                    level_feature=develop_phoc.extract_feature(sub,split)
+                    feature_vector=feature_vector+level_feature
 
+            return(feature_vector)  
+
+
+
+            
 
 
 
